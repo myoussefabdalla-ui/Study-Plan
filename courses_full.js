cb787239a11e3d205e3016d3fdcbd06d2c098ebe
myoussefabdalla-ui <m.youssef.abdalla@gmail.com>
2025-09-22 19:13:09 +0300

    Delete courses_full.js

--- courses_full.js
+++
@@ -1,9 +0,0 @@
-
-const COURSES = []
-  .concat(COURSES_CORE1)
-  .concat(COURSES_CORE2)
-  .concat(COURSES_ELEC1)
-  .concat(COURSES_ELEC2)
-  .concat(COURSES_ELECGEN)
-  .concat(COURSES_OTHERS)
-  .concat(COURSES_FUND);
