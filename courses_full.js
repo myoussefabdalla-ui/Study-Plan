6d3a729b156e3fc6a79ee31797eff36d16626334
myoussefabdalla-ui <m.youssef.abdalla@gmail.com>
2025-09-22 20:05:41 +0300

    Add files via upload

---
+++ courses_full.js
@@ -0,0 +1,8 @@
+  const COURSES = []
+    .concat(COURSES_CORE1)
+    .concat(COURSES_CORE2)
+    .concat(COURSES_ELECGEN)
+    .concat(COURSES_ELEC1)
+    .concat(COURSES_ELEC2)
+    .concat(COURSES_FUND)
+    .concat(COURSES_OTHERS);
